7eecd6eff000348c4f8b0e9913619579f43df74c
KuroLeg <aaalcaine1@gmail.com>
2025-04-15 13:32:26 -0700

    button dynamically changes when it's being clicked

--- src/sillySFX.py
+++ src/sillySFX.py
@@ -3,9 +3,6 @@ import pygame
 import sys
 
 
-
-
-
 def main():
     
     pygame.init()
@@ -17,18 +14,35 @@ def main():
     
     #Button Object
     class Button:
-        def __init__(self,text,x_pos,y_pos,enabled):
+        def __init__(self,text,x_pos,y_pos,color,enabled):
             self.text =text
             self.x_pos =x_pos
             self.y_pos =y_pos
+            #self.rect = self.get_rect(center=(self.x_pos,self.y_pos))
+            self.color = color
             self.enabled = enabled
             self.draw()
 
         def draw(self):
-            button_txt = txtFont.render(self.text, True, 'black')
-            button_rect = pygame.rect.Rect((self.x_pos,self.y_pos),(70,70))
-            pygame.draw.rect(screen , 'red', button_rect, 0,5)
+            if self.check_click():
+                button_rect = pygame.rect.Rect((self.x_pos,self.y_pos),(65,65))
+                button_txt = txtFont.render(self.text, True,'dark gray')
+            else:
+                button_txt = txtFont.render(self.text, True, 'black')
+                button_rect = pygame.rect.Rect((self.x_pos,self.y_pos),(70,70))
+            
+            
+            pygame.draw.rect(screen ,self.color, button_rect, 0,5)
             screen.blit(button_txt, (self.x_pos +3, self.y_pos +20))
+
+        def check_click(self):
+            mouse_pos = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()[0]
+            button_rect = pygame.rect.Rect((self.x_pos,self.y_pos),(70,70))
+            if click and button_rect.collidepoint(mouse_pos) and self.enabled:
+                return True
+            else:
+                return False
 
 
 
@@ -38,7 +52,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill(bg_color)
-        my_button = Button('Silly fart', 10,10, True)
+        my_button = Button('Silly fart', 10,10,(95,200,25),True)
+        #print(my_button.check_click())
         pygame.display.flip()
     pygame.quit()
 
