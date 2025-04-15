b6427652e032fd36dffe3294a63a3d3ef1dbb0f4
KuroLeg <aaalcaine1@gmail.com>
2025-04-15 11:50:45 -0700

    My code now produces a blank white screen that can be resized

--- src/sillySFX.py
+++ src/sillySFX.py
@@ -2,7 +2,20 @@
 import pygame
 
 def main():
-    pass
+    pygame.init()
+    pygame.display.set_caption('Silly SFX')
+    resolution =((500,500))
+    bg_color = pygame.Color(255,255,255)
+    screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(bg_color)
+        pygame.display.flip()
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
