d629359f8a39c02aa678a0341566e4756d1301ae
KuroLeg <aaalcaine1@gmail.com>
2025-04-22 15:07:25 -0700

    added a base color pallete, and made all 9 buttons, and added hotkey functionality to button 2

--- src/sillySFX.py
+++ src/sillySFX.py
@@ -11,6 +11,20 @@ def main():
     resolution =((500,500))
     bg_color = pygame.Color(54,24,56)
     screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+
+    #color Pallette for buttons
+    #-------------------------------------------------------------------
+    pal_red=(227,68,86)
+    pal_orange=(236,136,90)
+    pal_lightyellow =(218,241,123)
+    pal_green=(122,213,104)
+    pal_blue=(55,164,232)
+    pal_magenta=(176,62,154)
+    pal_purple=(122,49,151)
+    pal_violet=(76,57,123)
+    pal_lightblue =(59,207,236)
+    pal_lightgreen=(62,153,97)
+    #-------------------------------------------------------------------
     
     #Button Object
     class Button:
@@ -44,10 +58,12 @@ def main():
             else:
                 return False
 
-    channel = pygame.mixer.Channel(0)
+    #-------------------------------------------------------------------
     #This is where you choose the sound effect files to place per button(mp3,wav,aiff)
+    channel = pygame.mixer.Channel(0)
     sfx1 = pygame.mixer.Sound('Flapjack_Scream.mp3')
     sfx2 = pygame.mixer.Sound('Vine_Boom.mp3')
+    #-------------------------------------------------------------------
 
     running = True
     while running:
@@ -55,21 +71,32 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill(bg_color)
-        button01 = Button('Scream', 70,50,(227,68,86),True)
-        button02 = Button('Thud', 190,50,(122,213,104),True)
-        button03 = Button('sfx3',310,50,'yellow',True)
+        button01 = Button('Scream', 80,110,pal_red,True)
+        button02 = Button('Thud', 200,110,pal_orange,True)
+        button03 = Button('sfx3',320,110,pal_lightyellow,True)
+
+        button04 = Button('sfx4',80,230,pal_lightgreen,True)
+        button05 = Button('sfx5',200,230,pal_green,True)
+        button06 = Button('sfx6',320,230,pal_lightblue,True)
         
+        button07 = Button('sfx7',80,350,pal_blue,True)
+        button08 = Button('sfx8',200,350,pal_magenta,True)
+        button09 = Button('sfx9',320,350,pal_purple,True)
         #Code for the sounds to play
+        #---------------------------------------------
         if event.type == pygame.MOUSEBUTTONDOWN:
             if button01.check_click():
                 channel.play(sfx1)
             if button02.check_click():
                 channel.play(sfx2)
-
+        #---------------------------------------------
+       
         #Code for the Keyboard Shortcuts
         keys = pygame.key.get_pressed()
         if keys[pygame.K_1]:
             channel.play(sfx1)
+        if keys[pygame.K_2]:
+            channel.play(sfx2)
 
         pygame.display.flip()
     pygame.quit()
